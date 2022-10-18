dccd94dc169e28ada6d2400168795ef52f062445
magetech <magetechnologic@gmail.com>
2022-10-18 00:31:05 -0300

    Atualização de Título

---
+++ precache-manifest.c2a41a9371a619ab4969ecb633f7b376.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388e264aed0ca98139d296e08c590491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47df6a22015a834e6ff4",
+    "url": "/static/css/2.7987459a.chunk.css"
+  },
+  {
+    "revision": "42c8d6e2b653ca1d7acc",
+    "url": "/static/css/main.c46b3efa.chunk.css"
+  },
+  {
+    "revision": "47df6a22015a834e6ff4",
+    "url": "/static/js/2.7891e107.chunk.js"
+  },
+  {
+    "revision": "3b68484d2210d581b9d2",
+    "url": "/static/js/3.913b10fd.chunk.js"
+  },
+  {
+    "revision": "42c8d6e2b653ca1d7acc",
+    "url": "/static/js/main.eb165d4c.chunk.js"
+  },
+  {
+    "revision": "4d50d8243dec977f7589",
+    "url": "/static/js/runtime~main.a3f860bf.js"
+  },
+  {
+    "revision": "22f890ae8edaa9843e81cb66bec1e687",
+    "url": "/static/media/arte-logo.22f890ae.jpg"
+  },
+  {
+    "revision": "9b5dcf86e7a2655959f70c448309018c",
+    "url": "/static/media/bkg-logo.9b5dcf86.svg"
+  },
+  {
+    "revision": "71f733c4efaddc4269386e2f6bda8bae",
+    "url": "/static/media/logo-icon.71f733c4.svg"
+  },
+  {
+    "revision": "e33f5b79a9da2ce01b834eec19b6acd4",
+    "url": "/static/media/mobile-3.e33f5b79.svg"
+  },
+  {
+    "revision": "c490f2e56f0c74091251b6fa10a065dc",
+    "url": "/static/media/orcamento.c490f2e5.svg"
+  },
+  {
+    "revision": "140ec533110cfebb2ed561a56511a5c5",
+    "url": "/static/media/platform.140ec533.svg"
+  },
+  {
+    "revision": "26e9ada6b00218d6b4604dd12c434134",
+    "url": "/static/media/site.26e9ada6.svg"
+  }
+]);
